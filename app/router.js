2fce048a010f7773d1cb51201c7a6a199c4c7ff8
karim <mdmirajulkarim@gmail.com>
2019-12-07 20:52:43 +0200

    file create sevaral

---
+++ app/router.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class router extends Component {
+    render() {
+        return (
+            <View>
+                <Text> textInComponent </Text>
+            </View>
+        )
+    }
+}
